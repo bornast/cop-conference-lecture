8feaaccdedd128bdc926dcbc47cf17baf3980153
Borna Šunjić <borna.sunjic.st@gmail.com>
2025-09-02 10:06:41 +0200

    Refactor student server component

--- components/student-server.ts
+++ components/student-server.ts
@@ -1,11 +1,14 @@
 import * as aws from "@pulumi/aws";
 import * as fs from "fs";
 import * as pulumi from "@pulumi/pulumi";
+import { LogGroup } from "@pulumi/aws/cloudwatch";
 
-export interface StudentServerArgs {
+type StudentServerArgs = {
   students: string[];
+  pubKey: string;
   instanceType?: string;
-}
+  amiId?: string;
+};
 
 export class StudentServer extends pulumi.ComponentResource {
   public readonly publicIp: pulumi.Output<string>;
@@ -17,10 +20,36 @@ export class StudentServer extends pulumi.ComponentResource {
     opts?: pulumi.ComponentResourceOptions
   ) {
     super("edu:ec2:StudentServer", name, {}, opts);
-    const { students, instanceType = "t2.micro" } = args;
 
-    // ---------- Security Group ----------
-    const sg = new aws.ec2.SecurityGroup(
+    const { students, pubKey, amiId, instanceType = "t2.micro" } = args;
+    const securityGroup = this.createSecurityGroup(name);
+    const ami = amiId || this.getAMI().id;
+    const keyPair = this.createKeyPair(name, pubKey);
+    const instanceProfile = this.createInstanceProfile(name);
+    const logGroup = this.createLogGroup(name);
+    const userData = this.createUserDataLogs(logGroup, students);
+
+    const server = new aws.ec2.Instance(
+      `${name}-instance`,
+      {
+        instanceType,
+        ami,
+        keyName: keyPair.keyName,
+        vpcSecurityGroupIds: [securityGroup.id],
+        iamInstanceProfile: instanceProfile.name,
+        tags: { Name: `${name}-instance` },
+        userData,
+      },
+      { parent: this }
+    );
+
+    this.publicIp = server.publicIp;
+    this.publicDns = server.publicDns;
+    this.registerOutputs();
+  }
+
+  private createSecurityGroup(name: string) {
+    return new aws.ec2.SecurityGroup(
       `${name}-sg`,
       {
         description: "Allow SSH inbound",
@@ -38,27 +67,27 @@ export class StudentServer extends pulumi.ComponentResource {
       },
       { parent: this }
     );
+  }
 
-    // ---------- AMI ----------
-    const ami = aws.ec2.getAmiOutput({
+  private getAMI() {
+    return aws.ec2.getAmiOutput({
       mostRecent: true,
       owners: ["amazon"],
       filters: [{ name: "name", values: ["amzn2-ami-hvm-*-x86_64-gp2"] }],
     });
+  }
 
-    // ---------- Key Pair ----------
-    const pubKey = fs
-      .readFileSync(`${process.env.HOME}/.ssh/id_rsa.pub`)
-      .toString();
-    const key = new aws.ec2.KeyPair(
+  private createKeyPair(name: string, pubKey: string) {
+    return new aws.ec2.KeyPair(
       `${name}-key`,
       {
         publicKey: pubKey,
       },
       { parent: this }
     );
+  }
 
-    // ---------- IAM Role + Instance Profile ----------
+  private createInstanceProfile(name: string) {
     const cwRole = new aws.iam.Role(
       `${name}-cw-role`,
       {
@@ -70,7 +99,7 @@ export class StudentServer extends pulumi.ComponentResource {
     );
 
     new aws.iam.RolePolicyAttachment(
-      `${name}-cw-attach`,
+      `${name}-cw-policy`,
       {
         role: cwRole.name,
         policyArn: "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
@@ -85,83 +114,65 @@ export class StudentServer extends pulumi.ComponentResource {
       },
       { parent: this }
     );
+    return instanceProfile;
+  }
 
-    // ---------- CloudWatch Log Group ----------
-    const logGroup = new aws.cloudwatch.LogGroup(
+  private createLogGroup(name: string) {
+    return new aws.cloudwatch.LogGroup(
       `${name}-logs`,
       {
         retentionInDays: 7,
       },
       { parent: this }
     );
-
-    // ---------- UserData ----------
-    const userData = logGroup.name.apply((lgName) => {
-      let script = `#!/bin/bash
-set -e
-
-# Enable password authentication
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart sshd
-
-# Install CloudWatch Agent
-yum install -y amazon-cloudwatch-agent
-
-# Prepare log file
-touch /var/log/student-logs.log
-chmod 666 /var/log/student-logs.log
-
-# CloudWatch Agent config
-mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
-cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
-{
-  "logs": {
-    "logs_collected": {
-      "files": {
-        "collect_list": [
-          { "file_path": "/var/log/cloud-init-output.log", "log_group_name": "${lgName}", "log_stream_name": "{instance_id}" },
-          { "file_path": "/var/log/student-logs.log", "log_group_name": "${lgName}", "log_stream_name": "{instance_id}" }
-        ]
-      }
-    }
   }
-}
-EOF
 
-systemctl enable amazon-cloudwatch-agent
-systemctl restart amazon-cloudwatch-agent
-`;
+  private createUserDataLogs(logGroup: LogGroup, students: string[]) {
+    return logGroup.name.apply((lgName) => {
+      let script = `
+        #!/bin/bash
+        set -e
+
+        # Enable password authentication
+        sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+        systemctl restart sshd
+
+        # Install CloudWatch Agent
+        yum install -y amazon-cloudwatch-agent
+
+        # Prepare log file
+        touch /var/log/student-logs.log
+        chmod 666 /var/log/student-logs.log
+
+        # CloudWatch Agent config
+        mkdir -p /opt/aws/amazon-cloudwatch-agent/etc
+        cat <<EOF > /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+        {
+        "logs": {
+            "logs_collected": {
+            "files": {
+                "collect_list": [
+                { "file_path": "/var/log/cloud-init-output.log", "log_group_name": "${lgName}", "log_stream_name": "{instance_id}" },
+                { "file_path": "/var/log/student-logs.log", "log_group_name": "${lgName}", "log_stream_name": "{instance_id}" }
+                ]
+            }
+            }
+        }
+        }
+        EOF
+
+        systemctl enable amazon-cloudwatch-agent
+        systemctl restart amazon-cloudwatch-agent`;
 
       students.forEach((s) => {
         script += `
-id -u ${s} &>/dev/null || useradd -m -s /bin/bash ${s}
-echo "${s}:${s}" | chpasswd
-echo 'exec > >(tee -a /var/log/student-logs.log) 2>&1' >> /home/${s}/.bashrc
-chown ${s}:${s} /home/${s}/.bashrc
-`;
+        id -u ${s} &>/dev/null || useradd -m -s /bin/bash ${s}
+        echo "${s}:${s}" | chpasswd
+        echo 'exec > >(tee -a /var/log/student-logs.log) 2>&1' >> /home/${s}/.bashrc
+        chown ${s}:${s} /home/${s}/.bashrc`;
       });
 
-      return script;
+      return script.replace(/^[ \t]+/gm, "");
     });
-
-    // ---------- EC2 Instance ----------
-    const server = new aws.ec2.Instance(
-      `${name}-instance`,
-      {
-        instanceType,
-        ami: ami.id,
-        keyName: key.keyName,
-        vpcSecurityGroupIds: [sg.id],
-        iamInstanceProfile: instanceProfile.name,
-        tags: { Name: `${name}-server` },
-        userData: userData,
-      },
-      { parent: this }
-    );
-
-    this.publicIp = server.publicIp;
-    this.publicDns = server.publicDns;
-
-    this.registerOutputs();
   }
 }
